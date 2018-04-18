07f84ed037ec529645ff3e7d65839d3528066d6a
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-04-18 03:46:36 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,8 +42,8 @@ class Song
     self.all.sort_by{ |song| song.name }
   end
 
-  def self.new_from_filename
-
+  def self.new_from_filename(filename)
+    parts = filename.split(" - ")
   end
 
   def self.create_from_filename
