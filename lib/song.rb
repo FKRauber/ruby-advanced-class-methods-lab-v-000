658a87f62cfa793e801b348a358b06a97a85d878
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-04-18 03:36:42 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,8 +34,8 @@ class Song
     end
   end
 
-  def self.find_or_create_by_name
-
+  def self.find_or_create_by_name(song_name)
+    self.find_by_name(song_name) || self.create_by_name(song_name)
   end
 
   def self.alphabetical
